7c13dfa5cf44109ea3d471c6d5dbb70453e080ad
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-03-03 13:58:09 +0530

    Add project files.

---
+++ AwaitAsync/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwaitAsync
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            doJob();
+            simpledo();
+
+            MyTask myTask = new MyTask();
+            myTask.dotheJob().Wait();
+
+            Console.ReadKey();
+
+        }
+
+        private static async Task doJob()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("\n Method do the job");
+
+                    Task.Delay(1000).Wait();
+                }
+
+            });
+        }
+
+        private static void simpledo()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("\n Method simple do");
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+     
+
+        class MyTask {
+
+
+            public async Task dotheJob()
+            {
+                int a = await doSomethingAsync(10);
+                return;
+            }
+
+            private async Task<int> doSomethingAsync(int v)
+            {
+                return await Task.Run(() =>
+                {
+
+                    Thread.Sleep(1000);
+                    Console.WriteLine("\n doSomethingAsync");
+                    Console.Write("\n "+(v * 2));
+                    return v * 2;
+
+                });
+            }
+        }
+     
+    }
+
+
+}
+
+
+
+
